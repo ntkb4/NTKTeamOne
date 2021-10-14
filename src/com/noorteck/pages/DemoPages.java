2e9a007786f9d41d6a75bda1191fba9945bb24b6
ntkb4 <ntkjasur@gmail.com>
2021-10-14 08:24:25 -0400

    Initial Commit...

---
+++ src/com/noorteck/pages/DemoPages.java
@@ -0,0 +1,9 @@
+package com.noorteck.pages;
+
+public class DemoPages {
+	
+	public static void main(String[] args) {
+		System.out.println("Good morning Team");
+	}
+
+}
---
+++ src/com/noorteck/steps/StepsDemo.java
@@ -0,0 +1,8 @@
+package com.noorteck.steps;
+
+public class StepsDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Keep it up :)");
+	}
+}
