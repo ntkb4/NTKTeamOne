6e7b9b40949e77eb0873ba16df178bd0d8b1f264
user1 <user1@user.fios-router.home>
2021-10-18 20:32:31 -0400

    commit changes

--- src/com/noorteck/pages/DemoPages.java
+++ src/com/noorteck/pages/DemoPages.java
@@ -5,6 +5,7 @@ public class DemoPages {
 	public static void main(String[] args) {
 		System.out.println("Good morning Team");
 		System.out.println("Great job");
+		System.out.println("pam");
 	}
 
 }
